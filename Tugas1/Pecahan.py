cc90bae9a9aa87de619b144939022d3ad96d43a1
devanooo <trestantodevano@gmail.com>
2024-10-21 00:38:17 +0700

    baru selektor doang ngantuk

---
+++ Tugas1/Pecahan.py
@@ -0,0 +1,42 @@
+# Nama File: Pecahan.py
+# Deskripsi : 
+# Nama / NIM : Devano Trestanto / 24060124140149
+# Tanggal: / /
+
+# +=========================================================================+ #
+#                       Definisi dan Spesifikasi Selektor
+# +=========================================================================+ #
+
+# (variable) = (input) -> (output)
+# (detailed description)
+
+def bil(p):
+    return p[0]
+def n(p):
+    return p[1]
+def d(p):
+    return p[2]
+
+def pembilang(p):
+    return p[0]
+def penyebut(p):
+    return p[1]
+
+# +=========================================================================+ #
+#                        Definisi dan Spesifikasi Konstruktor
+# +=========================================================================+ #
+def makepecahancampuran(bil,pemcampur,penyecampur):
+    return[bil,pemcampur,penyecampur]
+
+def makepecahan(pembilang,penyebut):
+    return[pembilang,penyebut]
+
+
+# +=========================================================================+ #
+#                               Realisasi
+# +=========================================================================+ #
+
+
+# +=========================================================================+ #
+#                                Aplikasi
+# +=========================================================================+ #
