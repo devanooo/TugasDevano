4b95c0aff2ee001374551ea95be498385ee196d5
devanooo <trestantodevano@gmail.com>
2024-10-22 21:39:13 +0700

    operator blm selesai

--- Tugas1/Pecahan.py
+++ Tugas1/Pecahan.py
@@ -13,9 +13,9 @@
 def bil(p):
     return p[0]
 def n(p):
-    return p[1]
+    return p[1] #pembilang
 def d(p):
-    return p[2]
+    return p[2] #penyebut
 
 def pembilang(p):
     return p[0]
@@ -30,6 +30,22 @@ def makepecahancampuran(bil,pemcampur,penyecampur):
 
 def makepecahan(pembilang,penyebut):
     return[pembilang,penyebut]
+
+# +=========================================================================+ #
+#                         Definisi dan Spesifikasi Operator
+# +=========================================================================+ #
+
+def konversipecahan(P):
+    return makepecahan((d(P) * bil(P) + n(P)), d(P) )
+
+def konversireal(P):
+    return (d(P) * bil(P) + n(P) / d(P))
+
+def AddP(P1,P2):
+    return makepecahancampuran(bil(P1) + bil(P2),n(P1)*d(P2) + n(P2)*d(P1), d(P1)*d(P2))
+
+def SubP(P1,P2):
+    return makepecahancampuran(bil(P1) - bil(P2),n(P1)*d(P2) - n(P2)*d(P1), d(P1)*d(P2))
 
 
 # +=========================================================================+ #
